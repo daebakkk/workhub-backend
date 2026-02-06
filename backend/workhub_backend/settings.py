dfa2128de0f7fe1d43a07978d254fef2c6971f94
Abisola <abipriscilla2007@gmail.com>
2026-02-06 09:44:35 +0100

    fixes

--- backend/workhub_backend/settings.py
+++ backend/workhub_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-pgp#wlo2urf8&jb0p(vkqp7zbr(mlnq*)%9knu&q!rne8^t0gm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,11 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
